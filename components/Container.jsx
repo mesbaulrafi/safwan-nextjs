981add4d19ab890b4d8664d6380e81e56362bbdc
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-04-11 02:45:21 +0600

    first commit

---
+++ components/Container.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Container = (className,children) => {
+  return (
+    <div className={`w-292.5 mx-auto ${className} `} >{children}</div>
+  )
+}
+
+export default Container
