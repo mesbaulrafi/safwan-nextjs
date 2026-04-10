6817d8706294c87aef3df67fb8d8d338d7b1611c
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-04-11 03:27:47 +0600

    add font

--- layouts/Banner.jsx
+++ layouts/Banner.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <div>Banner</div>
+    <>
+    <div className="bg-[url('../public/bannerImg.png')] bg-no-repeat bg-center bg-cover py-52.5">
+    <h2 className='font-jost'>Hello! </h2>
+    <h2>Hello! </h2>
+    </div>
+    </>
   )
 }
 
