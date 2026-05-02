946bfc158ffe85fc1a8eebfcafb5fe9afe1c0152
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-05-03 02:04:54 +0600

    add service part

---
+++ layouts/Service.jsx
@@ -0,0 +1,16 @@
+import Container from '@/components/Container'
+import React from 'react'
+
+export default function Service() {
+  return (
+   <>
+   <section>
+    <div className="">
+        <Container>
+            <div className=""></div>
+        </Container>
+    </div>
+   </section>
+   </>
+  )
+}
