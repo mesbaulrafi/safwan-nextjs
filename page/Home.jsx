0f1c2f31d0f4d2638607b567b9c38f6d6a3e782d
Mesbaul Haque <mesbaulrafi@gmail.com>
2026-05-26 11:33:36 +0600

    add service card

--- page/Home.jsx
+++ page/Home.jsx
@@ -1,10 +1,12 @@
 import Banner from '@/layouts/Banner'
+import Service from '@/layouts/Service'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
       <Banner/>
+      <Service/>
     </div>
   )
 }
